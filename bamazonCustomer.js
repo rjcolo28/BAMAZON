cf6c2406eb31690153656f81df0687d8cb701344
Ryan Colosanti <silentcornball28@Ryans-MacBook-Pro.local>
2019-02-05 18:09:30 -0500

    installed inquirer package

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,9 +1,19 @@
 var mysql = require("mysql");
 
-var connect = mysql.createConnection({
+var con = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "R0ssm00r#25",
-    database: products
+    database: "bamazon"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("connected");
+    con.query("SELECT * FROM products", function(err, results) {
+        if (err) throw err;
+        console.log(results);
+    });
+    con.end();
 });
