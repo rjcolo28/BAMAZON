3504a1c402ebb72725da11e78c9c7a87fd7f8a87
Ryan Colosanti <silentcornball28@Ryans-MacBook-Pro.local>
2019-02-05 17:09:20 -0500

    mysql package installed and connected to mysql database

---
+++ bamazonCustomer.js
@@ -0,0 +1,9 @@
+var mysql = require("mysql");
+
+var connect = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "R0ssm00r#25",
+    database: products
+});
