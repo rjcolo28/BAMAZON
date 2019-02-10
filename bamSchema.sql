116919b72db0fec482b8661fb8046e7970e8ed35
Ryan Colosanti <silentcornball28@Ryans-MacBook-Pro.local>
2019-02-09 23:24:13 -0500

    Added rawlist prompt

---
+++ bamSchema.sql
@@ -0,0 +1,3 @@
+USE bamazon;
+
+SELECT * FROM products;
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,7 @@
 var mysql = require("mysql");
 
+var inquirer = require("inquirer")
+
 var con = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -8,12 +10,37 @@ var con = mysql.createConnection({
     database: "bamazon"
 });
 
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("connected");
-    con.query("SELECT * FROM products", function(err, results) {
+var command = process.argv[2];
+
+if (command === "customer") {
+    con.connect(function (err) {
         if (err) throw err;
-        console.log(results);
+        console.log("You are connected!");
     });
-    con.end();
-});
+    makePurchase();
+}
+
+function makePurchase() {
+    con.query("SELECT * FROM products", function (err, response) {
+        if (err) throw err;
+        console.log(response);
+        inquirer
+            .prompt([
+                {
+                    name: "purchase",
+                    type: "rawlist",
+                    choices: function () {
+                        var productArr = [];
+                        for (var i = 0; i < response.length; i++) {
+                            productArr.push(response[i].ID.toString());
+                        }
+                        return productArr;
+                    },
+                    message: "Choose what product you wish to purchase (by ID)."
+                }
+            ])
+            .then(function (answer) {
+                console.log(answer.purchase);
+            })
+    })
+}
